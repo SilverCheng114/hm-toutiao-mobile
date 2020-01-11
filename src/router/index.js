4b7322ba19907b3e1ba650ca425e6d9f56705e58
SilverCheng114 <204860795@qq.com>
2020-01-11 12:17:46 +0800

    配置整个项目路由规则

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,65 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '@/views/home'
-
+const Layout = () => import('@/views/layout') // 布局组件
+const Home = () => import('@/views/home') // 主页组件
+const Question = () => import('@/views/question') // 问答组件
+const Video = () => import('@/views/video') // 视频组件
+const User = () => import('@/views/user') // 个人中心
+const Profile = () => import('@/views/user/profile.vue') // 编辑资料组件
+const Chat = () => import('@/views/user/chat.vue') // 编辑资料组件
+const Login = () => import('@/views/login') // 登录组件
+const Article = () => import('@/views/article') // 文章组件
+const Search = () => import('@/views/search') // 搜索组件
+const Result = () => import('@/views/search/result') // 搜索结果
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        component: Home
+      },
+      {
+        path: '/question',
+        component: Question
+      },
+      {
+        path: '/video',
+        component: Video
+      },
+      {
+        path: '/user',
+        component: User
+      }
+    ]
+  },
+  {
+    path: '/user/profile',
+    component: Profile
+  },
+  {
+    path: '/user/chat',
+    component: Chat
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/article',
+    component: Article
+  },
+  {
+    path: '/search',
+    component: Search
+  },
+  {
+    path: '/search/result',
+    component: Result
   }
   // {
   //   path: '/about',
