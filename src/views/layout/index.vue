e73d90e8a5dea1b95073d2cdab3f6c84faef97f2
SilverCheng114 <204860795@qq.com>
2020-01-11 15:11:52 +0800

    完成layout布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,12 +1,55 @@
 <template>
-  <div>布局</div>
+  <div class="container">
+    <van-nav-bar
+      v-if="showNavbar"
+      fixed
+      title="黑马头条"
+      right-text="搜索"
+      @click-right="$router.push('/search')"
+    />
+    <!-- 二级路由 -->
+    <div class="my-wrapper" :class="{noTop: !showNavbar}">
+      <router-view></router-view>
+    </div>
+
+    <van-tabbar route>
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
+      <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'layout'
+  name: 'layout',
+  computed: {
+    showNavbar () {
+      return this.$route.path !== '/user'
+    }
+  },
+  methods: {
+    onClickRight () {}
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .my-wrapper {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop {
+      padding-top: 0;
+    }
+  }
+}
 </style>
