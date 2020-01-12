69ca4d55c43b98c6d89cf00fd690d14d8ef0c451
SilverCheng114 <204860795@qq.com>
2020-01-12 12:00:18 +0800

    修改主题蓝色

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './permission'
 import store from './store'
 import Vant from 'vant'
-import 'vant/lib/index.css' // vant样式文件
+import 'vant/lib/index.less' // vant样式文件
 import '@/styles/index.less' // 引入全局的自定义样式
 import 'amfe-flexible'
 Vue.use(Vant)
