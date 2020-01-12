9f9450059520a9a141ea3a9abcbafdfc7e280457
SilverCheng114 <204860795@qq.com>
2020-01-12 11:35:02 +0800

    登录页面基本布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="page-user-chat">
     <van-nav-bar left-arrow @click-left="$router.back()" title="登录"></van-nav-bar>
+    <!-- 手机号 验证码 登录按钮 -->
+    <van-cell-group>
+      <van-field label="手机号" placeholder="请输入手机号"></van-field>
+      <van-field label="验证码" placeholder="请输入验证码">
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+      <div class="btn-box">
+        <van-button type="info" size="small" block round>登 录</van-button>
+      </div>
+    </van-cell-group>
   </div>
 </template>
 
@@ -11,4 +21,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.btn-box{
+  padding: 20px;
+}
 </style>
